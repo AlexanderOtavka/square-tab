5bcf60ec2c23132daa41a31de1675ea1f0937fc2
Zander Otavka <zotavka@gmail.com>
2016-05-09 16:04:17 -0700

    Use target="_blank" instead of JS for GitHub link

--- scripts/popup.js
+++ scripts/popup.js
@@ -8,16 +8,10 @@ const {
 const $alwaysShowBookmarks = document.querySelector('#always-show-bookmarks');
 const $bookmarksDrawerSmall = document.querySelector('#bookmarks-drawer-small');
 const $boxedInfo = document.querySelector('#boxed-info');
-const $about = document.querySelector('#about');
 
 bindCheckbox($alwaysShowBookmarks, settings.keys.ALWAYS_SHOW_BOOKMARKS);
 bindCheckbox($bookmarksDrawerSmall, settings.keys.BOOKMARKS_DRAWER_SMALL);
 bindCheckbox($boxedInfo, settings.keys.BOXED_INFO);
-
-// About link
-$about.addEventListener('click', () => {
-  chrome.tabs.create({ url: $about.href });
-});
 
 function bindCheckbox($wrapper, settingKey) {
   const $checkbox = $wrapper.querySelector('input[type=checkbox]');
