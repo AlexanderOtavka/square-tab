ca3eaaddcbc6cd9be45a0fe4397fd3cc9766879b
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2018-12-12 16:27:19 +0100

    Tweak get sun info code

--- src/newtab/useWeather.js
+++ src/newtab/useWeather.js
@@ -2,20 +2,22 @@ import { useEffect, useState, useCallback } from "react"
 
 import useBehaviorSubject from "../util/useBehaviorSubject"
 
-const HOUR_MS = 60 * 60 * 1000
+const SECOND_MS = 1000
+const MINUTE_MS = 60 * SECOND_MS
+const HOUR_MS = 60 * MINUTE_MS
 const DAY_MS = 24 * HOUR_MS
 const DEFAULT_SUNRISE = 6 * HOUR_MS // 6am
 const DEFAULT_SUNSET = 18 * HOUR_MS // 6pm
 
-function getSunInfoMs(data, date) {
-  const tzOffset = date.getTimezoneOffset() * 60 * 1000
+const getSunInfoFromData = data => date => {
+  const tzOffset = date.getTimezoneOffset() * MINUTE_MS
   const now = (date.valueOf() - tzOffset) % DAY_MS
 
   let sunrise
   let sunset
   if (data && Date.now() < data.sunExpiration) {
-    sunrise = (data.sys.sunrise * 1000 - tzOffset) % DAY_MS
-    sunset = (data.sys.sunset * 1000 - tzOffset) % DAY_MS
+    sunrise = (data.sys.sunrise * SECOND_MS - tzOffset) % DAY_MS
+    sunset = (data.sys.sunset * SECOND_MS - tzOffset) % DAY_MS
   } else {
     sunrise = DEFAULT_SUNRISE
     sunset = DEFAULT_SUNSET
@@ -35,7 +37,7 @@ function getSunInfoMs(data, date) {
 
 export default function useWeather(weatherStore) {
   const data = useBehaviorSubject(weatherStore.dataSubject)
-  const getSunInfo = useCallback(date => getSunInfoMs(data, date), [data])
+  const getSunInfo = useCallback(getSunInfoFromData(data), [data])
   const [cacheIsLoaded, setCacheLoaded] = useState(false)
   useEffect(() => {
     weatherStore.cacheLoaded.then(() => setCacheLoaded(true))
